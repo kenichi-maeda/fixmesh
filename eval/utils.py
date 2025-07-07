400726ff042f59e5a94fcf82ba406b9c995532cf
kenichi-maeda <kenichi.maeda121@gmail.com>
2025-07-07 08:58:51 -0400

    Add meshlib again

--- eval/utils.py
+++ eval/utils.py
@@ -10,7 +10,7 @@ import trimesh
 from scipy.spatial import cKDTree
 from numpy.linalg import norm
 import pymeshfix
-# import meshlib.mrmeshpy as mrmesh
+import meshlib.mrmeshpy as mrmesh
 from scipy.sparse import lil_matrix
 
 def convert_to_pyvista(mesh):
--- fixmesh/self_intersections/__init__.py
+++ fixmesh/self_intersections/__init__.py
@@ -6,7 +6,7 @@ from .local_remesh import fix_with_localRemesh
 __all__ = [
     "fix_with_pymeshfix",
     "fix_with_pymesh",
-    # "fix_with_meshlib",
+    "fix_with_meshlib",
     "fix_with_surfaceNet",
     "fix_with_localRemesh"
 ]
